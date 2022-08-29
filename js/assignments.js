e325772df2f528fb1fa8e3df6b7342e15292b079
Emilio Benavides <emilio.benavides68@gmail.com>
2022-08-29 13:14:35 -0500

    Completed math operators

--- js/assignments.js
+++ js/assignments.js
@@ -23,11 +23,23 @@ const country = "USA";
 const continent = "North America";
 console.log(typeof isIsland);
 
-//basic operators
+//Math operators
 const now = 2037;
 const ageEmilio = now  - 1986;
 const ageSarah = now - 2018;
 console.log(ageEmilio, ageSarah);
+const firstName = 'Emilio';
+const lastName = 'Benavides';
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+let x = 10 +5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++;// x = x +1
+x--;
+console.log(x);
+
 
 console.log(ageEmilio * 2, ageEmilio / 10, 2 ** 3);
 
--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 let js = "Amazing";
 if (js === "Amazing") alert("javascript is fun!!!");
+//
+// console.log(40 + 8 + 23 -10);
+// console.log(23);
 
-console.log(40 + 8 + 23 -10);
-console.log(23);
+// let firstName = "Emilio";
+// let first = "mike";
+// let myFirstJob = "programmer";
+// let myCurrentJob = "Teacher";
 
-let firstName = "Emilio";
-let first = "mike";
-let myFirstJob = "programmer";
-let myCurrentJob = "Teacher";
-
-console.log(myCurrentJob);
-console.log(firstName);
+// console.log(myCurrentJob);
+// console.log(firstName);
 
 
 
