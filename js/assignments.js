cfa3f9e1026e95458dad81c3edc47bf276616669
Emilio Benavides <emilio.benavides68@gmail.com>
2022-08-29 12:29:09 -0500

    added js folder

---
+++ js/assignments.js
@@ -0,0 +1,39 @@
+"use strict";
+//Lecture one Values and variables
+// let country = "USA";
+// let continent = "North America";
+let population = 579;
+//
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+//Data Types lecture
+let isIsland = true;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// let, const and var lecture
+const language = "English";
+const country = "USA";
+const continent = "North America";
+console.log(typeof isIsland);
+
+//basic operators
+const now = 2037;
+const ageEmilio = now  - 1986;
+const ageSarah = now - 2018;
+console.log(ageEmilio, ageSarah);
+
+console.log(ageEmilio * 2, ageEmilio / 10, 2 ** 3);
+
+
+
+
+// console.log(population / 2);
+
+
---
+++ js/script.js
@@ -0,0 +1,19 @@
+let js = "Amazing";
+if (js === "Amazing") alert("javascript is fun!!!");
+
+console.log(40 + 8 + 23 -10);
+console.log(23);
+
+let firstName = "Emilio";
+let first = "mike";
+let myFirstJob = "programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myCurrentJob);
+console.log(firstName);
+
+
+
+
+
+
